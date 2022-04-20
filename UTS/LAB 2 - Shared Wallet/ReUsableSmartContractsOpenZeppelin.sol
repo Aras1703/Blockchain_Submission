aa0798dbeb658264fb5f5301f2abd5eb0c6aa73a
Arasy Bazwir <83108505+Aras1703@users.noreply.github.com>
2022-04-21 02:37:21 +0700

    Add files via upload

---
+++ UTS/LAB 2 - Shared Wallet/ReUsableSmartContractsOpenZeppelin.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.1;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract SharedWallet is Ownable {
+     function isOwner() internal view returns(bool) {
+        return owner() == msg.sender;
+     }
+
+     function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+        _to.transfer(_amount);
+     }
+
+     receive() external payable {
+     }
+}
